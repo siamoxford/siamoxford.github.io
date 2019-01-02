554de82dc93951618995252a82a0625ce36d0fb4
AmbroseYim <yim.ambrose@gmail.com>
2019-01-02 15:54:21 +0800

    modified contact form destination

--- php/contact.php
+++ php/contact.php
@@ -2,7 +2,7 @@
 $email = $_POST['senderemail'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "siamsc@maths.ox.ac.uk";
+$recipient = "oxfordsiamchapter@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
